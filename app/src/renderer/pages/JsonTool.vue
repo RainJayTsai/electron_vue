a63f4b3f35da0995d424c404f9431fc32a52ee9c
RainJay <rainjay0117@gmail.com>
2017-05-15 14:59:13 +0800

    fix electron fs bug and change ip

--- app/src/renderer/pages/JsonTool.vue
+++ app/src/renderer/pages/JsonTool.vue
@@ -11,7 +11,10 @@
   import FileInput from 'components/FileInput'
   //import dialog from 'electron'
   import rename from 'fs'
+  //http://stackoverflow.com/questions/37994441/how-to-use-fs-module-inside-electron-atom-webpack-application
   const {dialog} = require('electron').remote
+  var remote = require('electron').remote;
+  var fs = remote.require('fs');
 
   export default{
     data() {
@@ -26,13 +29,13 @@
     methods: {
         doSubmit(file_path, has_header) {
             {
-                this.$http.post('http://98e5945f.ngrok.io', {yaml: this.yaml, file_path, has_header}).then(response => {
+                this.$http.post('http://127.0.0.1:8787/api', {yaml: this.yaml, file_path, has_header}).then(response => {
                 // get body data
                 console.log(response.body.file_path);
                 dialog.showSaveDialog({
                     title: 'Save to location'
                 }, (dstpath) => {
-                    rename(file_path, dstpath, () => {
+                    fs.rename(response.body.file_path, dstpath, () => {
                         console.log('OK');
                     });
                 });
