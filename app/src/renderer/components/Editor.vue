3a4e65e8852638680cddc5246337503bf6a0bffb
rainjay <RainJay0117@gmail.com>
2017-06-07 22:44:10 +0800

    init test

--- app/src/renderer/components/Editor.vue
+++ app/src/renderer/components/Editor.vue
@@ -14,20 +14,82 @@
 </template>
 
 <script>
-    function insertContent(val, that) {
-        let textareaDom = document.querySelector('.mdEditor');
-        let value = textareaDom.value;
-        let point = range.getCursortPosition(textareaDom);
-        let lastChart = value.substring(point - 1, point);
-        let lastFourCharts = value.substring(point - 4, point);
-        if (lastChart != '\n' && value != '' && lastFourCharts != '    ') {
-            val = '\n' + val;
-            range.insertAfterText(textareaDom, val);
-        } else {
-            range.insertAfterText(textareaDom, val);
-        }
-        that.input = document.querySelector('.mdEditor').value;
-    }
+//    function getCursortPosition(textDom) {
+//        var cursorPos = 0;
+//        if (document.selection) {
+//            // IE Support
+//            textDom.focus();
+//            var selectRange = document.selection.createRange();
+//            selectRange.moveStart('character', -textDom.value.length);
+//            cursorPos = selectRange.text.length;
+//        } else if (textDom.selectionStart || textDom.selectionStart == '0') {
+//            // Firefox support
+//            cursorPos = textDom.selectionStart;
+//        }
+//        return cursorPos;
+//    }
+//
+//    function setCaretPosition(textDom, pos) {
+//        if (textDom.setSelectionRange) {
+//            // IE Support
+//            textDom.focus();
+//            textDom.setSelectionRange(pos, pos);
+//        } else if (textDom.createTextRange) {
+//            // Firefox support
+//            var range = textDom.createTextRange();
+//            range.collapse(true);
+//            range.moveEnd('character', pos);
+//            range.moveStart('character', pos);
+//            range.select();
+//        }
+//    }
+//    function getSelectText() {
+//        var userSelection, text;
+//        if (window.getSelection) {
+//            // Firefox support
+//            userSelection = window.getSelection();
+//        } else if (document.selection) {
+//            // IE Support
+//            userSelection = document.selection.createRange();
+//        }
+//        if (!(text = userSelection.text)) {
+//            text = userSelection;
+//        }
+//        return text;
+//    }
+//    function insertAfterText(textDom, value) {
+//        var selectRange;
+//        if (document.selection) {
+//            // IE Support
+//            textDom.focus();
+//            selectRange = document.selection.createRange();
+//            selectRange.text = value;
+//            textDom.focus();
+//        } else if (textDom.selectionStart || textDom.selectionStart == '0') {
+//            // Firefox support
+//            var startPos = textDom.selectionStart;
+//            var endPos = textDom.selectionEnd;
+//            var scrollTop = textDom.scrollTop;
+//            textDom.value = textDom.value.substring(0, startPos) + value + textDom.value.substring(endPos, textDom.value.length);
+//            textDom.focus();
+//            textDom.selectionStart = startPos + value.length;
+//            textDom.selectionEnd = startPos + value.length;
+//            textDom.scrollTop = scrollTop;
+//        } else {
+//            textDom.value += value;
+//            textDom.focus();
+//        }
+//    }
+//    function insertContent(val, that) {
+//        let textareaDom = document.querySelector('.mdEditor');
+//        let value = textareaDom.value;
+//        console.log(value)
+//        let point = getCursortPosition(textareaDom);
+//        let lastChart = value.substring(point - 1, point);
+//        let lastFourCharts = value.substring(point - 4, point);
+//        insertAfterText(textareaDom, val);
+//        that.context = document.querySelector('.mdEditor').value  ;
+//    }
     export default{
         data(){
             return {
@@ -39,11 +101,11 @@
         },
         methods: {
             indent(e) {
-//                this.context = this.context.slice(0, e.srcElement.selectionStart) +
-//                    '    ' + this.context.slice(e.srcElement.selectionStart);
-//
-//                e.srcElement.selectionEnd = 0;
-                insertContent("    ",this)
+                this.context = this.context.slice(0, e.srcElement.selectionStart) +
+                    '    ' + this.context.slice(e.srcElement.selectionStart);
+
+                e.srcElement.selectionEnd = 0;
+//                insertContent("    ",this);
             }
         },
         watch: {
@@ -58,8 +120,7 @@
 
 <style>
     textarea {
-        width: 80%;
-        max-width: 400px;
+        width: 600px;
         height: 80%;
         min-height: 450px;
         resize: none;
--- app/src/renderer/components/FileInput.vue
+++ app/src/renderer/components/FileInput.vue
@@ -1,9 +1,18 @@
 <template>
     <div>
         <form @submit.prevent="onSubmit">
-        <input type="file" name="points" @change="onFileChange"/>
-        <div>You Select File is <input type="text" v-model="file_path"></div>
-        <div><span class="has-header">Has csv header: <input type=checkbox v-model="has_header"></span><input type=submit value="Submit"> <input type=submit value="Default"></div>
+
+            <input type="file" name="points" @change="onFileChange" class="custom-file-input"/>
+            <!--<div>You Select File is <input type="text" v-model="file_path"></div>-->
+            <div>
+                <span class="has-header" style="font-size: 20px">Has csv header:
+                    <input type=checkbox v-model="has_header">
+                </span>
+                <!--<input class="btn btn-success" type=submit value="Default">-->
+                <!--<input class="btn btn-primary" type=submit value="Submit">-->
+                <input type=submit value="Default">
+                <input type=submit value="Submit">
+            </div>
         </form>
     </div>
 </template>
@@ -33,8 +42,7 @@
 </script>
 
 <style>
-.has-header
-{
+.has-header {
     margin: 1em;
 }
 </style>
